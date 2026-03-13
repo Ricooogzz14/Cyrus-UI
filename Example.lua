5724acec13c407d8034742c0c7399ec63d5e44ae
Ricooogzz14 <ricardonorton19@gmail.com>
2026-03-13 17:29:52 +0000

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -8,6 +8,59 @@ local Window = CyrusUI.CreateWindow({
 --/ SET THEME
 CyrusUI.SetTheme("Purple")
 
+Theme Name       | Type
+-----------------|----------------
+Cyan             | Basic
+Purple           | Basic
+Green            | Basic
+Orange           | Basic
+Red              | Basic
+White            | Basic
+Blue             | Basic
+Pink             | Basic
+Lime             | Basic
+Gold             | Basic
+NeonBlue         | Effects
+NeonGreen        | Effects
+NeonPink         | Effects
+NeonPurple       | Effects
+NeonOrange       | Effects
+Midnight         | Advanced
+Obsidian         | Advanced
+Shadow           | Advanced
+Void             | Advanced
+Stealth          | Advanced
+PastelBlue       | Basic
+PastelPink       | Basic
+PastelGreen      | Basic
+PastelPurple     | Basic
+PastelOrange     | Basic
+Ocean            | Basic
+Forest           | Basic
+Sunset           | Basic
+Rose             | Basic
+Ice              | Basic
+Cyber            | Advanced
+Matrix           | Advanced
+Terminal         | Advanced
+Circuit          | Advanced
+TechPurple       | Advanced
+Rainbow          | Effects
+Galaxy           | Effects
+Fire             | Effects
+Electric         | Effects
+Plasma           | Effects
+Crimson          | Basic
+Emerald          | Basic
+Sapphire         | Basic
+Amethyst         | Basic
+Amber            | Basic
+NeonRainbow      | Effects
+Aurora           | Effects
+Firestorm        | Effects
+Frostbite        | Effects
+SolarFlare       | Effects
+
 --/ CREATE A NOTIFICATION
 CyrusUI.Notify({
     Title = "Hello",
